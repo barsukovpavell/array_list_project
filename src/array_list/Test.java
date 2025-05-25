945639bd1dbdb1d0dce42edc1bec0582788783d6
barsukovpavell <barsukovpavell@gmail.com>
2025-05-25 22:47:50 +0300

    A anb B

--- src/array_list/Test.java
+++ src/array_list/Test.java
@@ -7,10 +7,25 @@ public class Test {
 
         ArrayList<Integer> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i <= 30; i++) {
             arrayList.add(i);
         }
 
         System.out.println(arrayList);
+
+        System.out.println(arrayList.get(0));
+        System.out.println(arrayList.get(29));
+
+        //a:
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.print(arrayList.get(i) + " ");
+        }
+
+        System.out.println("\n");
+
+        //b:
+        for (Integer x : arrayList) {
+            System.out.print(x);
+        }
     }
 }
