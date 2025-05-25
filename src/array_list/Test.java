0ae0e38805ce7ca823c83afe6034db4dd1f47f02
barsukovpavell <barsukovpavell@gmail.com>
2025-05-25 18:29:44 +0300

    Add array_list/Test

--- src/array_list/Test.java
+++ src/array_list/Test.java
@@ -1,4 +1,16 @@
 package array_list;
 
+import java.util.ArrayList;
+
 public class Test {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println(arrayList);
+    }
 }
