9bd31519c04379dbd18f29ff35a1bc4f8aeb0ae2
barsukovpavell <barsukovpavell@gmail.com>
2025-05-28 11:07:53 +0300

    Measure time

---
+++ src/linke_list/Test.java
@@ -0,0 +1,33 @@
+package linke_list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> linkeList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkeList);
+        measureTime(arrayList);
+    }
+
+    private static void measureTime(List<Integer> List) {
+        for (int i = 0; i < 1000000; i++) {
+            List.add(i);
+        }
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000000; i++) {
+            List.get(i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+    }
+}
+
+
