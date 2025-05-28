86767201d6f23bda34bc28d580746854daab16c9
barsukovpavell <barsukovpavell@gmail.com>
2025-05-28 22:45:50 +0300

    my lynked list. add get/set.

--- src/linke_list/Test.java
+++ src/linke_list/Test.java
@@ -17,7 +17,6 @@ public class Test {
         for (int i = 0; i < 1000000; i++) {
             List.add(i);
         }
-
         long start = System.currentTimeMillis();
 
         for (int i = 0; i < 1000000; i++) {
